4f0e828e0e4b7447e725aaed840b0ac7e1a895c9
adnankhan46 <technicaljigsaw.tj@gmail.com>
2024-02-28 18:43:59 +0530

    Donate Done

--- src/components/Donate/Donate.js
+++ src/components/Donate/Donate.js
@@ -3,6 +3,9 @@ import "./Donate.css";
 import { Fade } from "react-reveal";
 import { Slide } from 'react-slideshow-image';
 import 'react-slideshow-image/dist/styles.css'
+import "owl.carousel/dist/assets/owl.carousel.css";
+import "owl.carousel/dist/assets/owl.theme.default.css";
+import OwlCarousel from "react-owl-carousel";
 
 
 const spanStyle = {
@@ -141,6 +144,7 @@ const Donate = () => {
           <li>Help Children from marginalised communities get Quality Education</li>
           <li>Help those who are worst effected during emergencies</li>
           <li>Help to Install Sanitary Pad Disperser Machine in Government School & Colleges Based in Rural Areas</li>
+          <li>Help underprivileged children to secure their future and help them shine</li>
           </ul>
         {/** Img Carousel */}
 
@@ -348,6 +352,51 @@ const Donate = () => {
             </a>
           </div>
         </div>
+         {/**Section 3 */}
+      <div className="section6">
+      <h1>Testimonials</h1>
+                    
+                    <OwlCarousel
+                      className="owl-theme"
+                      nav={false}
+                      items={1} 
+                      margin={40}
+                      autoplay={true}
+                      autoplayTimeout={2000}
+                      autoplayHoverPause={true}
+                      loop={true}
+                      dots={true}
+                    >
+                      {/* Testimonial 1 */}
+                      <div className="singleDo">
+                        <div className="testContent singleDo pink">
+                        <p>I've always wanted to donate towards the betterment of the society and i think STARLIGHT FOUNDATION is the best. Platform to start with.
+                        Within a week of lockdown thousands of families have no food in their plates & are still struggling It is almost like our moral duty to help such needfull one's.
+                        The best part is working with so many like minded people for the noble cause & being able to something about the problem I fell we share hope among those who need it the most. </p>
+                          <h4 className="doTitle">~Megha Urkude (Volunteer)</h4>
+                        </div>
+                      </div>
+              
+                      {/* Testimonial 2 */}
+                      <div className="singleDo">
+                        <div className="testContent singleDo orange">
+                        <p>In my journey with STARLIGHT FOUNDATION, I learnt the importance of the little things we take for granted and from these children, I learnt what the exact meaning. of being happy is. Sometimes little things can make a huge difference. Most importantly, I understood that one should never circumstance is. stop dreaming, no matter what the STARLIGHT FOUNDATION made me a more humble
+                        and empathizing person. Proud to be a part of this by which I made a clear mindset of not complaining about the setbacks and to face all the challenges no matter what!! </p>
+                          <h4 className="doTitle">~Shaswat Pandey(Volunteer)</h4>
+                        </div>
+                      </div>
+              
+                      {/* Testimonial 3 */}
+                      <div className="singleDo">
+                        <div className="testContent singleDo blue">
+                        <p>I had no idea where to place mine focus and create a positive change in my community. Had various ideas for brainstorming but had no actual direction. However one thing was certain, that I wanted to work with children in foster care. Mostly I wanted to create a "Bright spot" and show the children at the centre that they were important part of the community.
+                        an STARLIGHT FOUNDATION was the only platform what I was dreaming for.
+                        I felt delighted working with such co-operative volunteers having such good mind set of helping nation to complete the journey of so called a "Developed Nation." </p>
+                          <h4 className="doTitle">~Sukriti Tiwari (Volunteer)</h4>
+                        </div>
+                      </div>
+                    </OwlCarousel>
+                    </div>
       </div>
     </React.Fragment>
   );
